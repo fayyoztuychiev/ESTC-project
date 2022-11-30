1e0c0e90ac12e45b9028c661612a91f7f1fda875
fayyoztuychiev <115862491+fayyoztuychiev@users.noreply.github.com>
2022-11-30 18:46:16 +0500

    Update main.c

--- Workshop 4: PWM and double click/main.c
+++ Workshop 4: PWM and double click/main.c
@@ -1,1 +1,35 @@
+#include "led_blinking_func.h"
+#include "button.h"
+#include "pwm.h"
+
+/**
+ * @brief Function for application main entry.
+ */
+
+int main(void)
+{
+    init_board_leds();
+    init_board_button();
+    nrfx_systick_init();
+    lfclk_config();
+    gpiote_init();
+    app_timer_init();
+    create_timers();
+
+    while (true)
+    {
+        for (int i = 1; i <= LEDS_NUMBER; i++)
+        {
+            for (int j = 1; j <= led_blinking_times(i); j++)
+            {
+                smoothly_blinking_mode(i);
+                nrf_delay_ms(100);
+            }
+        }
+    }
+}
+
+/**
+ *@}
+ **/
 
